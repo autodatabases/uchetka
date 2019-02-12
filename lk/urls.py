24fb102d1ed028b53e975a28b9561972dc23b60b
Kov4egg <mailkyda@gmail.com>
2019-02-12 16:34:21 +0400

    Create stocks page

--- lk/urls.py
+++ lk/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', rediredct_detal_list),
+    path('', rediredct_detal_list_page),
     path('add_stockroom/', add_StockRoom),
     path('add_auto/', include('add_auto.urls')),
     path('detals_list/', include('detals_list.urls')),
+    path('stocks/', include(('stocks.urls', 'stocks'), namespace='stocks ')),
 ]
